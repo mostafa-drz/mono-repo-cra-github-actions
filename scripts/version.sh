82753a3cc9566313eeb70ed9d719690c56835866
mostafa.drz <mostafa69d@gmail.com>
2020-08-16 17:54:06 -0400

    let's see if the version is accessible

--- scripts/version.sh
+++ scripts/version.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 
-export LONGVERSION=$(git describe --tags --long --abbrev=8 --always HEAD || :)$(echo -"$DRONE_BRANCH" | tr / - | grep -v '\-master' || :)
+export LONGVERSION=$(git describe --tags --long --abbrev=8 --always HEAD || :)$(echo -"${GITHUB_REF##*/}" | tr / - | grep -v '\-master' || :)
 export VERSION=${DRONE_TAG:-$LONGVERSION}
-export REACT_APP_VERSION=${DRONE_TAG:-$LONGVERSION}
+export REACT_APP_VERSION=${github.sha:-$LONGVERSION}
